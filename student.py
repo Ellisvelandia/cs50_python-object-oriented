32fa6c135c33bc7249c79af96ae8c00c7ea464a0
Ellisvelandia <eyis619l@gmail.com>
2023-08-08 20:24:22 -0500

    tuples

--- student.py
+++ student.py
@@ -1,15 +1,19 @@
 def main():
-  name = get_name()  
-  house = get_house()
-  print(f"{name} from {house}")
+  student = get_students()
+  if student[0] == "Padma":
+    student[1] = "Ravenclaw"
+  print(f"{student[0]} from {student[1]}")
+
+def get_students():
+  name = input("Name: ")
+  house = input("House: ")
+  return [name, house]
+# def get_name():
+#   return input("Name: ")
 
 
-def get_name():
-  return input("Name: ")
-
-
-def get_house():
-  return input("Name: ")
+# def get_house():
+#   return input("Name: ")
 
 if __name__ == "__main__":
   main()  
