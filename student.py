01f3d74efad2d029827599b8280bf7d5209525de
Ellisvelandia <eyis619l@gmail.com>
2023-08-10 07:47:16 -0500

    instance method

--- student.py
+++ student.py
@@ -1,28 +1,35 @@
 class Student:
-   def __init__(self, name , house):
-     self.name = name
-     self.house = house
+    def __init__(self, name, house):
+        if not name:
+            # print("Missing name")
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff","Ravenclaw","Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
 
 
 def main():
-  student = get_students()
-  print(f"{student.name} from {student.house}")
-  # if student[0] == "Padma":
-  #   student[1] = "Ravenclaw"
-  # print(f"{student[0]} from {student[1]}")
+    student = get_students()
+    print(f"{student.name} from {student.house}")
+    # if student[0] == "Padma":
+    #   student[1] = "Ravenclaw"
+    # print(f"{student[0]} from {student[1]}")
+
 
 def get_students():
-  name = input("Name: ")
-  house = input("House ")
-  student = Student(name, house)
-  # student = Student()
-  # student.name = input("Name: ")
-  # student.house = input("House ")
-  return student
-  # student = {}
-  # student["name"] = input("Name: ")
-  # student["house"] = input("House: ")
-  # return student
+    name = input("Name: ")
+    house = input("House ")
+    return Student(name, house)
+    
+    # student = Student()
+    # student.name = input("Name: ")
+    # student.house = input("House ")
+    # return student
+    # student = {}
+    # student["name"] = input("Name: ")
+    # student["house"] = input("House: ")
+    # return student
 # def get_name():
 #   return input("Name: ")
 
@@ -31,4 +38,4 @@ def get_students():
 #   return input("Name: ")
 
 if __name__ == "__main__":
-  main()  
+    main()
